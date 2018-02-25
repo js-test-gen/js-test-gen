85085ca8b62f2ee560995199258ccff99ad7c17f
grady-lad <ogradymartin7@gmail.com>
2018-02-25 13:27:47 +1100

    Refactoring/renaming "utils" to "template" for template generation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import prettier from "prettier";
 import parser from "./parser";
 import {
-  spreadMods,
-  generateTestCases,
-  generateNamedImports,
-  generateDefaultImport,
-  generateFromStatement
-} from "./utils";
+  testTemplate,
+  testFileTemplate,
+  namedModTemplate,
+  defaultTemplate
+} from "./templates";
 
-export const generateTest = (contents = "") => {
-  const { namedMods, defaultMod } = parser(contents);
-  return prettier.format(generateTestCases(spreadMods(namedMods, defaultMod)));
-};
+export const generateTest = (contents = "") => testTemplate(parser(contents));
 
 export const generateTestTemplate = ({
   contents = "",
@@ -20,28 +15,16 @@ export const generateTestTemplate = ({
 }) => {
   const { namedMods, defaultMod } = parser(contents, srcFileName);
   const areNameMods = (mods = []) => mods.length > 0;
+  const fileDetails = { srcFileName, importFromPath };
   //Template Gen if default and named exist in file.
   if (defaultMod && areNameMods(namedMods)) {
-    return prettier.format(`
-      import ${generateDefaultImport(defaultMod.name)},
-      ${generateNamedImports(namedMods)}
-      ${generateFromStatement(srcFileName, importFromPath)}
-      ${generateTestCases(spreadMods(namedMods, defaultMod))};
-    `);
+    return testFileTemplate({ defaultMod, namedMods }, fileDetails);
   }
   if (defaultMod) {
-    return prettier.format(`
-      import ${generateDefaultImport(defaultMod.name)}
-      ${generateFromStatement(srcFileName, importFromPath)}
-      ${generateTestCases([defaultMod])};
-    `);
+    return defaultTemplate(defaultMod, fileDetails);
   }
   if (areNameMods(namedMods)) {
-    return prettier.format(`
-      import ${generateNamedImports(namedMods)}
-      ${generateFromStatement(srcFileName, importFromPath)}
-      ${generateTestCases(namedMods)};
-    `);
+    return namedModTemplate(namedMods, fileDetails);
   }
   return "";
 };
---
+++ src/templates/index.js
@@ -0,0 +1,73 @@
+import prettier from "prettier";
+
+export const spreadMods = (namedMods = [], defMod) => {
+  return defMod ? [defMod, ...namedMods] : namedMods;
+};
+
+export const generateTestCases = (mods = []) => {
+  const initialTemplate = "";
+  return mods.reduce(
+    (template, mod) => `${template} describe("${mod.name}", () => {
+      it("should fail auto generated test", () => {
+          expect(${mod.name}()).toBe(false);
+      });
+    });`,
+    initialTemplate
+  );
+};
+
+export const generateNamedImports = (funcList = []) => {
+  const namedImports = funcList.reduce((exportStr, func) => {
+    return `${exportStr} ${func.name},`;
+  }, "{");
+  const removeLastComma = str => {
+    return str.slice(0, str.length - 1);
+  };
+  // namedImports will be "{" if reducing over empty array
+  return namedImports.length > 1 ? `${removeLastComma(namedImports)} }` : "";
+};
+
+export const generateDefaultImport = (defaultImport = "") => defaultImport;
+
+export const generateFromStatement = (fileName = "", fromPath) => {
+  const location = fromPath || `./${fileName}`;
+  return `from '${location}'`;
+};
+
+export const testFileTemplate = (
+  { defaultMod, namedMods },
+  { srcFileName, importFromPath }
+) => {
+  return prettier.format(`
+    import ${generateDefaultImport(defaultMod.name)},
+    ${generateNamedImports(namedMods)}
+    ${generateFromStatement(srcFileName, importFromPath)}
+    ${generateTestCases(spreadMods(namedMods, defaultMod))};
+  `);
+};
+
+export const namedModTemplate = (
+  namedMods,
+  { srcFileName, importFromPath }
+) => {
+  return prettier.format(`
+    import ${generateNamedImports(namedMods)}
+    ${generateFromStatement(srcFileName, importFromPath)}
+    ${generateTestCases(namedMods)};
+  `);
+};
+
+export const defaultTemplate = (
+  defaultMod,
+  { srcFileName, importFromPath }
+) => {
+  return prettier.format(`
+    import ${generateDefaultImport(defaultMod.name)}
+    ${generateFromStatement(srcFileName, importFromPath)}
+    ${generateTestCases([defaultMod])};
+  `);
+};
+
+export const testTemplate = ({ defaultMod, namedMods }) => {
+  return prettier.format(generateTestCases(spreadMods(namedMods, defaultMod)));
+};
